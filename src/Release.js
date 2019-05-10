8065ed60f82ddc1f85f661476fe0689bde21f953
Jerome Lachaud <jerome.lachaud@gmail.com>
2019-05-10 09:53:05 +0200

    Add link to the release page

--- src/Release.js
+++ src/Release.js
@@ -1,16 +1,19 @@
+/* eslint-disable react/jsx-no-duplicate-props */
 import React from 'react'
 const { baseUrl } = require('./config/app')
 
 export const Release = props => {
-  const { artists_sort, released_formatted, styles, title } = props
+  const { artists_sort, released_formatted, styles, title, uri } = props
   return (
     <div>
       <h1>{title}</h1>
-      <img
-        src={`${baseUrl}/images/`}
-        alt=""
-        style={{ marginBottom: '1.25rem' }}
-      />
+      <a rel="stylesheet" href={uri} target="_blank" rel="noopener noreferrer">
+        <img
+          src={`${baseUrl}/images/`}
+          alt=""
+          style={{ marginBottom: '1.25rem' }}
+        />
+      </a>
       <ul>
         <li />
         <li>{artists_sort}</li>
